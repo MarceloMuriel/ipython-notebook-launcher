044cde4a9801fafdfa7be5c2d774c0df25a7e44c
Marcelo Muriel <marcelomuriel@gmail.com>
2013-10-18 11:09:23 +0200

    admin notebooks params updated

--- launch-admin-notebook.sh
+++ launch-admin-notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog --port=8999 --no-browser --notebook-dir=/var/imi/prog/notebooks/nbprog --ip=130.125.78.14 &
-/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog --port=8998 --no-browser --notebook-dir=/var/imi/prog/notebooks/all --ip=130.125.78.14 &
+/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog --port=8999 --no-mathjax --no-browser --port-retries=0 --notebook-dir=/var/imi/prog/notebooks/nbprog --ip=130.125.78.14 &
+/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog --port=8998 --no-mathjax --no-browser --port-retries=0 --notebook-dir=/var/imi/prog/notebooks/all --ip=130.125.78.14 &
 # Launch it with a user who only has read permissions.
-#/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog_read --port=9000 --no-browser --notebook-dir=/var/imi/prog/notebooks/nbprog --ip=130.125.78.14 &
+#/usr/bin/python3 /usr/local/bin/ipython3 notebook --profile=nbprog_read --port=9000 --no-mathjax --no-browser --port-retries=0 --notebook-dir=/var/imi/prog/notebooks/nbprog --ip=130.125.78.14 &
